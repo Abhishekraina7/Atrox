40a58fe2e25da3a08aa90468a71a42465598ae51
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2026-04-27 19:06:41 +0530

    Removed the navcontroller and moved it to separate file for better state hoisting

--- Atrox/app/src/main/java/com/example/atrox/MainActivity.kt
+++ Atrox/app/src/main/java/com/example/atrox/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.example.atrox.navigation.AtroxNavHost
 import com.example.atrox.ui.theme.AtroxTheme
 import dagger.hilt.android.AndroidEntryPoint
 
