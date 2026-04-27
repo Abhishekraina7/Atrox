412fe24810924b5eec0e1356d28a86e23a88ce24
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2026-04-27 19:08:18 +0530

    Minor Changes

--- Atrox/app/src/main/java/com/example/atrox/AtroxApp.kt
+++ Atrox/app/src/main/java/com/example/atrox/AtroxApp.kt
@@ -2,10 +2,9 @@ package com.example.atrox
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.atrox.navigation.AtroxNavHost
+import com.example.atrox.ui.navigation.AtroxNavHost
 
 @Composable
 fun AtroxApp(
