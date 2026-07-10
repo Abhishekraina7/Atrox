a37f467a21b6aae76fea39858598600faa84cbcd
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2026-07-10 22:43:46 +0530

    added leakCanary library for checking the memory leaks in the app during the development

--- Atrox/app/build.gradle.kts
+++ Atrox/app/build.gradle.kts
@@ -141,4 +141,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // --- LeakCanary ---
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.14")
 }
